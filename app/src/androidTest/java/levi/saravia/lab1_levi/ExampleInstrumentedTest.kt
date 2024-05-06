8f9f0fe6d134d2f0301114448851d65a547b8bea
Levi Saravia <20230170@ricaldone.edu.sv>
2024-05-06 11:37:20 -0600

    Guardar Lab1

---
+++ app/src/androidTest/java/levi/saravia/lab1_levi/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package levi.saravia.lab1_levi
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("levi.saravia.lab1_levi", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/levi/saravia/lab1_levi/MainActivity2asdfgh.kt
@@ -0,0 +1,20 @@
+package levi.saravia.lab1_levi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2asdfgh : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_activity2asdfgh)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
